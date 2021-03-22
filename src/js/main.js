ebcb360f84419b8036be38a0b62f6e68909cd0df
adelakim <diddnjs02@likelion.org>
2021-03-22 17:25:48 +0900

    [Add] webpack-dev-middleware
    - scss 테스트
    - express, webpack-dev-middleware 설치
    - 서버 테스트 완료

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import "../style/style.scss";
+
+const main = document.createElement("div");
+
+document.body.appendChild(main);
+main.classList.add("square");
--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,14 @@ module.exports = {
   mode: "development", // production ? develop ? 선택
   // prod: 기본적으로 설정해주는 default 들이 있음
   // dev: 그게 없음 -> 찾아볼 것
-  entry: "./src/main.js", // 번들링 시작점을 알려주는 곳
+  entry: "./src/js/main.js", // 번들링 시작점을 알려주는 곳
   // js 파일을 읽어들이는 것이므로  js 파일로 해야함
   devtool: "inline-source-map",
   output: {
     // 번들링 후 파일이 어디로 저장되는지
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[hash].js",
+    publicPath: "http://localhost:9000",
   },
   module: {
     // 번들하고 싶은 파일인데 웹팩이 못알아듣는 애들을 위해 ex. sass

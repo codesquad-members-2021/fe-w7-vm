05b76926a1a45c46bac44591de966d0f7899955d
Juddroid <nobllizdc@gamil.com>
2021-03-22 16:27:28 +0900

    [EnvSetting] 환경세팅

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/app.html',
+    }),
+  ],
+};

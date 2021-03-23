304995f16cf1ed3a0eb54bc818140cbf26696679
cchoongh <cndgh49@naver.com>
2021-03-23 14:03:30 +0900

    Implement 'MoneySumView' class's prototype

---
+++ src/vending-machine/procress-screen/MoneySumView.js
@@ -0,0 +1,39 @@
+import './style.scss';
+import { _ } from '../../util.js';
+
+export class MoneySumView {
+  constructor() {
+    this.$target;
+    this.$sum;
+    this.init();
+  }
+
+  init() {
+    this.$target = this.createEl();
+    this.$sum = _.$('.sum', this.$target);
+  }
+
+  createEl() {
+    return _.genEl('DIV', {
+      classNames: ['money-sum'],
+      template: this.template({ sum : 0, currency: '원' }),
+    });
+  }
+
+  setSum(sum) {
+    this.$sum.textContent = sum;
+  }
+
+  getSum() {
+    return parseInt(this.$sum.textContent);
+  }
+
+  getEl() {
+    return this.$target;
+  }
+
+  template({ sum, currency }) {
+    return `<span class="sum">${sum}</span>
+            <span class="currency">${currency}</span>`
+  }
+}

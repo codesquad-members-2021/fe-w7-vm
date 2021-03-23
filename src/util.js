82961f155b05e0c3225d2708f8450e2710c820b7
cchoongh <cndgh49@naver.com>
2021-03-23 15:39:58 +0900

    Add 'clearChildren' function

--- src/util.js
+++ src/util.js
@@ -1,12 +1,16 @@
 export const _ = {
   $: (selector, base = document) => base.querySelector(selector),
   $All: (selector, base = document) => base.querySelectorAll(selector),
-  genEl: (tagName, { classNames, template, attributes } = {}) => {
+  genEl: (tagName, { classNames, template, attributes } = {}) => { // classNames: array
     const $el = document.createElement(tagName);
     if (classNames) $el.classList.add(...classNames);
     if (template) $el.innerHTML = template;
     if (attributes) Object.entries(attributes).forEach(([k, v]) => $el.setAttribute(k, v));
     return $el;
+  },
+  clearChildren: ($target) => {
+    while ($target.firstChild)
+      $target.firstChild.remove();
   },
   pipe: (...fns) => arg => fns.reduce((prevResult, fn) => fn(prevResult), arg),
   throttle: (fn, ms) => {

eeaaea14f103f9095977cad10a9a5632fe5cd67c
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-25 01:26:13 +0900

    Feat: 구매 가능 아이템을 보내주는 이벤트 발생시키는 기능 작성

--- src/vending-machine/products/ProductContainerModel.js
+++ src/vending-machine/products/ProductContainerModel.js
@@ -10,9 +10,12 @@ export default class ProductContainerModel extends Observable{
         this.menuInfo = json;
     }
     notifySelectedItem(idx) {
+        if(this.menuInfo.list[idx].stock == 0) return;
         this.reduceSum(idx);
         this.printSelectedLog(idx);
+        this.reduceStock(idx);
     }
+    // progressScreenView에 보냄 - 투입금액 줄이기
     reduceSum(i) {
         this.dispatchEvent(
             new CustomEvent('reduce-money-sum', {
@@ -20,11 +23,33 @@ export default class ProductContainerModel extends Observable{
             })
         );
     }
+    // logView에 보냄 - 선택, 배출 로그 출력
     printSelectedLog(i) {
         this.dispatchEvent(
             new CustomEvent('print-selected', {
                 detail: { name: this.menuInfo.list[i].name }
             })
         );
+    }
+    reduceStock(i) { 
+        this.menuInfo.list[i].stock--;
+    }
+    // ProductView에 보냄 - 구매가능 아이템 하이라이트
+    sendAvailableItems(moneySum) {
+        const itemIdxArr = this.getAvailableItems(moneySum);
+        if(itemIdxArr.length > 0) {
+            this.dispatchEvent(
+                new CustomEvent('render-available-items', {
+                    detail: { itemIdxArr: itemIdxArr }
+                })
+            );
+        }
+    }
+    getAvailableItems(moneySum) {
+        let itemIdx = [];
+        this.menuInfo.list.forEach((e, i) => {
+            if(e.price <= moneySum) itemIdx.push(i)
+        })
+        return itemIdx;
     }
 }

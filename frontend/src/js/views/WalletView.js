c103e8b299f2df2d56163d348b323c42c74f4996
Hannah Park <ha3158987@gmail.com>
2021-03-26 00:04:10 +0900

    Feat: render decreasing value of currency count

--- frontend/src/js/views/WalletView.js
+++ frontend/src/js/views/WalletView.js
@@ -5,22 +5,47 @@ class WalletView {
         this.walletModel = walletModel;
         this.walletViewWrapper = _.$(walletReference.walletWrapSelector);
         this.budgetTotalContainer = _.$(walletReference.budgetTotalSelector);
-        this.currencyTypes = walletModel.currencyTypes;
-
+        this.budgetData;
         this.init();
     }
 
     init = () => {
         this.renderBudgetInfo();
-        // this.walletModel.subscribe()
+        this.walletModel.subscribe(); //동전 개수 빠지기, 토탈금액 변경하기, 현재투입금액총합 보여주기, 진행상태 알려주기, productView에 구입가능 제품 활성화
     }
 
-    renderBudgetInfo = () => {
-        const countNodes = _.$all('.currency-count');
+    addClickEvent = () => {
+        const currencyBtns = _.$all(".currency-btn");
+        currencyBtns.forEach((button) => {
+            _.addEvent(button, 'click', (event) => {
+                this.decreaseCount(event);
+            });
+        })
+    }
+
+    decreaseCount = (event) => {
+        const clickedButton = event.target;
+        const clickedButtonType = _.getId("data-id", clickedButton);
+        this.budgetData.forEach((currencyPair) => {
+            if (JSON.stringify(currencyPair.type) === clickedButtonType && currencyPair.count) {
+                currencyPair.count--;
+                this.updateCount(clickedButton, currencyPair.count);
+            }
+        })
+    }
+
+    updateCount = (target, count) => {
+        const countContainer = target.nextElementSibling;
+        countContainer.textContent = `${count}개`;
+    }
+
+    renderBudgetInfo = async () => {
         this.walletModel.createBudget();
-        this.renderInitView(this.walletViewWrapper, this.walletModel.budgetData);
+        this.budgetData = this.walletModel.budgetData;
         const budgetTotal = this.walletModel.totalBudget;
+        this.renderInitView(this.walletViewWrapper, this.budgetData);
         this.budgetTotalContainer.textContent = `${this.addCommaToNumber(budgetTotal)}원`;
+        await this.addClickEvent();
     }
 
     renderInitView = (walletViewWrapper, currencyTypes) => {
@@ -35,7 +60,7 @@ class WalletView {
     createWalletHTML = (currencyType, count) => {
         const html = `
         <li class="wallet-grid-item">
-            <button type="button" class="m-auto py-1 rounded btn btn-success">${currencyType}원</button>
+            <button type="button" class="currency-btn m-auto py-1 rounded btn btn-success" data-id="${currencyType}">${currencyType}원</button>
             <span class="currency-count bg-white m-auto py-1 border rounded">${count}개</span>
         </li>
         `;

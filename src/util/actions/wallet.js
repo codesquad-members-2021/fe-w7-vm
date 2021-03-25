2d30be1ba33ce814d44e96f43838499fcddb207f
Jaeuk-YOO <ryou9305@naver.com>
2021-03-25 21:39:20 +0900

    fix: 액션 유틸로 다루도록 변경

--- src/util/actions/wallet.js
+++ src/util/actions/wallet.js
@@ -1,12 +1,11 @@
-const ADD_MONEY = "wallet/ADD_MONEY";
-const OUT_MONEY = "wallet/OUT_MONEY";
+import * as ACTION from "../enums/action.js"
 
 const addMoney = (item) => {
-  return { type: ADD_MONEY, payload: item }
+  return { type: ACTION.ADD_MONEY, payload: item }
 }
 
 const outMoney = (item) => {
-  return { type: OUT_MONEY, payload: item }
+  return { type: ACTION.OUT_MONEY, payload: item }
 }
 
 export { addMoney, outMoney };

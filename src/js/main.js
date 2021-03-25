d7dd625647ee74b9ab21f2db0136f28a4d1fec3b
adelakim <diddnjs02@likelion.org>
2021-03-25 22:10:48 +0900

    [Chore] Edit magic-number in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import "../style/style.scss";
 import { createSectionWithClassname } from "./DOM.js";
 import { _ } from "./utils.js";
-import { VIEWS, NUMBERS } from './variables.js';
+import { VIEWS, NUMBERS } from "./variables.js";
 import ProductView from "./view/ProductView.js";
 import WalletView from "./view/WalletView.js";
 import ProcessView from "./view/ProcessView.js";
@@ -10,10 +10,9 @@ import ProductModel from "./model/ProductModel.js";
 
 const app = _.$("#app");
 
-
 function init() {
   const timer = {
-    count : 0
+    count: 0,
   };
 
   Object.values(VIEWS).forEach((e) => createSectionWithClassname(app, e));
@@ -24,12 +23,11 @@ function init() {
   new ProcessView(walletModel, productModel, _.$(`.${VIEWS.PROCESS}`));
   setInterval(() => {
     timer.count++;
-    if(timer.count >= 5) {
+    if (timer.count >= NUMBERS.TERM) {
       walletModel.returnBalance();
       timer.count = 0;
     }
-  }, 1000);
-
+  }, NUMBERS.INITMS);
 }
 
 init();

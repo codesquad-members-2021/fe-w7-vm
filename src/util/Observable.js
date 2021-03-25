f44fbdbab06586ae1b0bb5b966a617ba1b73ac75
cchoongh <cndgh49@naver.com>
2021-03-25 11:36:32 +0900

    Modify 'Observable'

--- src/util/Observable.js
+++ src/util/Observable.js
@@ -1,9 +1,29 @@
 export default class Observable {
+  constructor() {
+    this.eventHandlerMap = new Map();
+  }
+
   subscribeEvent({ event, callback }) {
-    this.addEventListener(event, callback);
+    if (!this.eventHandlerMap.has(event))
+      this.eventHandlerMap.set(event, []);
+
+    this.eventHandlerMap.get(event).push(callback);
   }
 
   unsubscribeEvent({ event, callback }) {
-    this.removeEventListener(event, callback);
+    if (!this.eventHandlerMap.has(event))
+      throw new Error('Invalid arguments');
+  
+    const eventHandlers = this.eventHandlerMap.get(event);
+    const callbackIdx = eventHandlers.findIndex(callback);
+
+    if (callbackIdx === -1)
+      throw new Error('Invalid arguments');
+
+    eventHandlers.splice(callbackIdx, 1);
   }
+
+  dispatchEvent(customEventObj) {
+    this.eventHandlerMap.get(customEventObj.type)?.forEach(callback(customEventObj));
+  };
 }

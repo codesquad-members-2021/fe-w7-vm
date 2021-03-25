ae94dd8450feeac62576554296dc49d900f477e5
Jaeuk-YOO <ryou9305@naver.com>
2021-03-25 17:37:06 +0900

    add: store 적용 코드 (현재 레거시인 state가 남아있음)

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ import WalletContainer from "./components/Wallet/WalletContainer.js";
 import GoodsContainer from "./components/Goods/GoodsContainer.js";
 import ProcessContainer from "./components/process/processContainer.js";
 
-import { Coke, PineappleFanta, Cider } from "./util/objects/goods.js";
+import store from "./util/store/root.js";
+import {useStore, useDispatch, useSelector} from "./util/store/useStore.js";
+
+import { Coke, Cider, PineappleFanta, GrapeFanta, LemonAde, BonBon, CocoaJuice, CokeZero, PowerAde, ChocoMilk1, ChocoMilk2, ChocoMilk3 } from "./util/objects/goods.js";
+import { addItem, outItem } from "./util/actions/goods.js";
+import { addMoney, outMoney } from "./util/actions/wallet.js";
 
 class App {
   constructor({ $target }) {
@@ -12,7 +17,9 @@ class App {
     this.wallet = null;
     this.goods = null;
     this.process = null;
-
+    
+    this.store = store;
+    useStore(store); // store initialize
     // root state 
     // 향후 모델로 옮겨집니다.
     this.state = {
@@ -25,6 +32,15 @@ class App {
         coke: Array(10).fill(new Coke()),
         cider: Array(10).fill(new Cider()),
         pineappleFanta: Array(10).fill(new PineappleFanta()),
+        grapeFanta: Array(10).fill(new GrapeFanta()),
+        lemonAde: Array(10).fill(new LemonAde()),
+        bonbon: Array(10).fill(new BonBon()),
+        cocoaJuice: Array(10).fill(new CocoaJuice()),
+        cokeZero: Array(10).fill(new CokeZero()),
+        powerAde: Array(10).fill(new PowerAde()),
+        chocoMilk1: Array(10).fill(new ChocoMilk1()),
+        chocoMilk2: Array(10).fill(new ChocoMilk2()),
+        chocoMilk3: Array(10).fill(new ChocoMilk3()),
       },
 
       process: {
@@ -35,8 +51,40 @@ class App {
       },
 
     }
+    this.init()
     this.setState({});
   }
+  init() {
+    
+    let dispatch = useDispatch("goods");
+    const goodsState = useSelector((state) => state.goods);
+
+    // goods 추가 과정
+    const beverages = [Coke, Cider, PineappleFanta, GrapeFanta, LemonAde, BonBon, CocoaJuice, CokeZero, PowerAde, ChocoMilk1, ChocoMilk2, ChocoMilk3];
+    beverages.forEach((item) => {
+      [...Array(10).keys()].forEach((_) => {
+        const beverage = new item();
+        console.log(beverage.name)
+        const payload = { [beverage.name]: beverage }
+        dispatch( addItem(payload) );
+      })
+    })
+    console.log(goodsState.getState())
+    
+    // wallet 추가 과정
+    dispatch = useDispatch("wallet");
+    const moneys = [10, 50, 100, 500, 1000, 5000, 10000];
+    moneys.forEach((item) => {
+      [...Array(10).keys()].forEach((_) => {
+        const payload = { [item]: item }
+        dispatch( addMoney(payload) );
+      })
+    })
+    const walletState = useSelector((state) => state.wallet);
+    console.log(walletState.getState())
+    
+  }
+
   setState({ type, method, value }) { // destructured value is new state
     switch (type) {
       case "wallet":
@@ -93,7 +141,7 @@ class App {
     this.setState(state);
   }
 
-  // handleChangeGoods({ method, value }) {}
+  handleChangeGoods({ method, value }) {}
 
   render() {
     
@@ -108,12 +156,12 @@ class App {
       handleChangeGoods: this.handleChangeGoods.bind(this),
     })
 
-    this.process = new ProcessContainer({
-      $target: this.$target,
-      type: this.state.process.type,
-      method: this.state.process.method,
-      // item : this.state.process.item
-    })
+    // this.process = new ProcessContainer({
+    //   $target: this.$target,
+    //   type: this.state.process.type,
+    //   method: this.state.process.method,
+    //   // item : this.state.process.item
+    // })
   }
 }
 

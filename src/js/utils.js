03dbda3694177853f0350f5f43ea76f1292291ef
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-25 19:52:19 +0900

    [Add] Variables
    - utils에서 variables 분리

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,23 +3,9 @@ export const _ = {
   $$: (selector, element = document) => element.querySelectorAll(selector),
 };
 
-export const url = {
-  prod: "http://localhost:9000/prod",
-};
-
 export const request = async (url) => {
   const data = await fetch(url);
   return data.json();
-};
-
-export const currency = {
-  units: [10, 50, 100, 500, 1000, 5000, 10000],
-};
-
-export const NUMBERS = {
-  INITNUM: 0,
-  DEFAULTVALUE: 100000,
-  BUYPRODUCT: 2000
 };
 
 export const getRandom = (value) => parseInt(Math.random().toFixed(2) * value + 1000);
---
+++ src/js/variables.js
@@ -0,0 +1,18 @@
+export const NUMBERS = {
+  INITNUM: 0,
+  DEFAULTVALUE: 100000,
+  BUYPRODUCT: 2000,
+  LIMITSEC: 5000
+};
+
+export const TYPES = [10, 50, 100, 500, 1000, 5000, 10000];
+
+export const URL = {
+  PROD: "http://localhost:9000/prod",
+};
+
+export const VIEWS = {
+  PRODUCT: "productView",
+  PROCESS: "processView",
+  WALLET: "walletView",
+};

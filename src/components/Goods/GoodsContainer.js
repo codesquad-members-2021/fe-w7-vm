a23e6ad06deb21f59067a1f2d93fb956d284108e
Jaeuk-YOO <ryou9305@naver.com>
2021-03-25 17:36:32 +0900

    add: 상품 컨테이너 모델 사용하는 형태로 수정

--- src/components/Goods/GoodsContainer.js
+++ src/components/Goods/GoodsContainer.js
@@ -1,13 +1,13 @@
 import ItemContainer from "./Item/ItemContainer.js";
+import { useSelector } from "../../util/store/useStore.js";
 
 class GoodsContainer {
-  constructor({$target, goods}) {
+  constructor({$target}) {
     this.$target = $target;
     this.items = [];
 
     // state
-    console.log(goods)
-    this.goods = goods;
+    this.goods = useSelector((state) => state.goods.getState());
     
     this.setState();
   }
@@ -15,16 +15,17 @@ class GoodsContainer {
   setState() {
     this.render();
   }
+
   render() {
-    console.log(this.$target)
     const $goods = document.createElement("ul");
     $goods.className = "goods"
 
-    for(const name in this.goods) {
+    for (const name in this.goods) {
       const $good = document.createElement("li");
       const item = new ItemContainer({
         $target: $good,
         name: name, 
+        korean: this.goods[name][0].korean,
         amount: this.goods[name].length,
       });
       
--- src/components/Goods/Item/ItemContainer.js
+++ src/components/Goods/Item/ItemContainer.js
@@ -1,23 +1,27 @@
 import ItemPresentational from "./ItemPresentational.js";
 import * as ITME_ENUM from "../../../util/enums/item.js";
-// import "./item.scss";
+
+import { useDispatch, useSelector } from "../../../util/store/useStore.js";
+import { outItem } from "../../../util/actions/goods.js";
+
+import "./item.scss";
 
 // const STATUS = ["default", "isAbleToBuy", "isSoldOut"];
 
 class ItemContainer {
-  constructor({ $target, name, amount }) {
+  constructor({ $target, name, korean, amount }) {
     this.$target = $target;
     this.presentational = null;
 
     // state
     this.amount = null;
     this.name = name;
+    this.korean = korean;
     this.status = ITME_ENUM.STATUS.default;
     
-    this.setState({
-      type: "amount", value: amount
-    });
+    this.setState({ type: "amount", value: amount });
   }
+
   setState({type, value}) {
     switch(type) {
       case "amount":
@@ -39,14 +43,18 @@ class ItemContainer {
   }
 
   isSelected(value) {
+    const dispatch = useDispatch("goods");
     console.log("isSelected value", value);
-    // 옵저버블의 dispatch() 이벤트로 향후 갈 것이다.
+    
+    dispatch( outItem(value) )
+    
   }
 
   render() {
     this.presentational = new ItemPresentational({
       $target: this.$target, 
       name: this.name,
+      korean: this.korean,
       status: this.status,
       isSelected: this.isSelected.bind(this)
     });

587ffb120dbd2b615902fc360870fcb2570de4da
adelakim <diddnjs02@likelion.org>
2021-03-26 21:21:01 +0900

    [Refactor] Refactoring
    - 뷰에서 모델에 직접 접근하지 않고, 함수로 접근하는 방식으로 수정

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -8,6 +8,31 @@ export default class Timer {
     this.timerHtml = null;
   }
 
+  getCount() {
+    return this.count;
+  }
+
+  getTarget() {
+    return this.target;
+  }
+
+  getTimerHtml() {
+    return this.timerHtml;
+  }
+
+  setTextIntoTimerHtml(text) {
+    this.timerHtml.innerText = text;
+  }
+
+  toggleClassOfTimerHtml(flag, classname) {
+    if (flag === "remove") this.timerHtml.classList.remove(classname);
+    if (flag === "add") this.timerHtml.classList.add(classname);
+  }
+
+  setCount(value) {
+    this.count = value;
+  }
+
   init(walletModel) {
     this.render();
     this.timerHtml = _.$(".timer");
